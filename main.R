57f27eb8bd02ed6a5ac41c8094476cd16f446e5b
brian <briankin86@gmail.com>
2020-12-09 11:00:32 +0300

    Initial commit

---
+++ main.R
@@ -0,0 +1,5 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: brian
+# Created on: 11/18/2020
+
---
+++ sample.R
@@ -0,0 +1,5 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: brian
+# Created on: 11/18/20
+ mean(mtcars$mpg)
